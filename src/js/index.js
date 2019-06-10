a91c8f68aa5c90f1e9fb404eb4bcd79f9d1ae5c0
iamdoomling <belen@fibury.com>
2019-06-10 02:11:18 -0300

    búsqueda sugerida searchbar y búsqueda con cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// Constantes útiles
+
 const apiKey = 'JQhP1sBxi7d1SKpBsMlFDJYPGUobpcpK';
 const apiBaseUrl = 'http://api.giphy.com/v1/gifs/';
 
@@ -7,7 +9,8 @@ const apiBaseUrl = 'http://api.giphy.com/v1/gifs/';
 
 const searchBar = document.getElementById('search-bar')
 const searchButton = document.getElementById('search-button')
-const suggestedTopics = ['jonathanVanness', 'sailormercury', 'vapowave', 'glitter']
+const suggestedTopics = ['jonathan Van Ness', 'Sailor Mercury', 'vapowave', 'glitter']
+const suggestionWrapper = document.getElementsByClassName('search-suggestion-wrapper')[0]
 
 searchBar.addEventListener('input', event => {
     
@@ -18,14 +21,26 @@ searchBar.addEventListener('input', event => {
         searchButton.classList.remove('button-active')
         searchButton.classList.add('button-disabled')
     }
-});
+
+    suggestionWrapper.classList.remove('hidden')
+})
+
+suggestionWrapper.addEventListener('mousedown', e => {
+    searchAndAppendGifs(e.target.innerHTML)
+    suggestionWrapper.classList.add('hidden');
+})
+
+searchBar.addEventListener('blur', () => {
+    suggestionWrapper.classList.add('hidden');
+})
+
+// Search bar
 
 function getSearchResults(search) {
     const found = fetch(apiBaseUrl + 'search?q=' + search + '&api_key=' + apiKey) 
         .then((response) => {
            return response.json()
         }).then(data => {
-            console.log(data)
             return data
         })
         .catch((error) => {
@@ -35,7 +50,6 @@ function getSearchResults(search) {
 }
 
 function searchAndAppendGifs(value) {
-
     const previousContainer = document.getElementById('results');
     const textBox = document.getElementById('results-text')
 
@@ -61,7 +75,6 @@ function searchAndAppendGifs(value) {
             error.innerHTML = 'No se encontraron resultados para tu búsqueda'
             container.appendChild(error)
         }
-
     })
 }
 
@@ -76,6 +89,10 @@ searchBar.addEventListener('keydown', (event) => {
     }
 })
 
+// Sugested cards
+
+const cardWrapper = document.getElementsByClassName('cards-wrapper')[0]
+
 function suggestedCards (topics) {
 
     topics.forEach(topic => {
@@ -84,22 +101,32 @@ function suggestedCards (topics) {
             cardBox.classList.add('box')
             cardBox.classList.add('card')
             cardBox.innerHTML = `<div class="box-header">
-                                    <span>#${result.data[0].title}</span>
+                                    <span>#${topic}</span>
                                     <div class="flex-center-content">
                                         <img class="close-button" src="./../../public/images/close.svg"/>
                                     </div>
                                 </div>
                                 <div class="card-content">
-                                    <img src="${result.data[0].images.fixed_height.url}" class="card-img"/>
+                                    <img src="${result.data[0].images.fixed_height.url}" class="card-img" data-search="${topic}"/>
+                                    <div class="card-see-more button" data-search="${topic}">Ver más...</div>
                                 </div>`
 
-            document.getElementsByClassName('cards-wrapper')[0].appendChild(cardBox)
+            cardWrapper.appendChild(cardBox)
         })
     })
 }
 
 window.addEventListener('load', () => {
     suggestedCards(suggestedTopics)
+
+})
+
+cardWrapper.addEventListener('click', event => {
+    if (event.target !== event.currentTarget) {
+        const search = event.target.dataset.search
+        searchAndAppendGifs(search)
+    }
+    event.stopPropagation();
 })
 
 // Trending
